73a7a84f93dde4de0b21139ff1e61f17a598e3b2
victor <victor_raony12@hotmail.com>
2023-01-10 09:02:33 -0300

    Methods

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import http from 'node:http';
 
+//Metodos: GET, POST, PUT, PATCH, DELETE
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação especifica de um recurso no back-end
+// DELETE => Deletar um recurso no back-end
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello word Victor!')
+  const { method, url } = req
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuarios')  
+  } 
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuarios')
+  }
+  return res.end('Teste!')
 })
 
 server.listen(3333)
+
