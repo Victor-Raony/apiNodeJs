afa1c7701fccb44b33172980c91e126a5c4f4751
victor <victor_raony12@hotmail.com>
2023-01-11 08:28:33 -0300

    Salvando Users em Memoria/Headers

--- src/server.js
+++ src/server.js
@@ -7,12 +7,28 @@ import http from 'node:http';
 // PATCH => Atualizar uma informação especifica de um recurso no back-end
 // DELETE => Deletar um recurso no back-end
 
+// Stateful = aplicação salva em memoria 
+// Stateless = aplicação não salava em memoria 
+
+// JSON - JavaScript Object Notation
+
+// Cabeçalhos (Requisição/respostas ) => Metadados
+
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuarios')  
+    return res
+    .setHeader('Content-Type', 'application/json')
+    .end(JSON.stringify(users))  
   } 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Don',
+      email: 'johndoe@example.com',
+    })
     return res.end('Criação de usuarios')
   }
   return res.end('Teste!')
